3b895ddbbdc063bfaefbfc78abd083639c14ffda
Steve Tooke <steve@boxjump.co.uk>
2014-10-09 10:25:35 +0100

    Make 1 bottle work

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,5 +1,9 @@
 class Bottles
   def verse(remaining)
-    "#{remaining} bottles of beer on the wall, #{remaining} bottles of beer.\nTake one down and pass it around, #{remaining - 1} bottles of beer on the wall.\n"
+    if remaining == 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    else
+      "#{remaining} bottles of beer on the wall, #{remaining} bottles of beer.\nTake one down and pass it around, #{remaining - 1} bottles of beer on the wall.\n"
+    end
   end
 end
--- bottles/spec/bottles_spec.rb
+++ bottles/spec/bottles_spec.rb
@@ -15,7 +15,6 @@ describe "singing 99 bottles of beer" do
   end
 
   it "can sing about 1 bottle" do
-    skip
     expected = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     expect( song.verse(1) ).to eq( expected )
   end
