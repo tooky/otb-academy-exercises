7fe577caf91299bc03592a0a6cd0fbf6a9afe971
Steve Tooke <steve@boxjump.co.uk>
2014-10-09 10:46:00 +0100

    Make 2 bottles as close to the general verse as possible

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -18,8 +18,8 @@ class Bottles
       "1 bottle of beer on the wall, 1 bottle of beer.\n" <<
         "Take it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" <<
-        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+      "#{remaining} bottles of beer on the wall, #{remaining} bottles of beer.\n" <<
+        "Take one down and pass it around, #{remaining - 1} bottle of beer on the wall.\n"
     else
       "#{remaining} bottles of beer on the wall, #{remaining} bottles of beer.\n" <<
         "Take one down and pass it around, #{remaining - 1} bottles of beer on the wall.\n"
