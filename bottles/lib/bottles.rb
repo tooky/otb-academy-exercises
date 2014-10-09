20cd801d73e977d706bce958da59c6c3079fa925
Steve Tooke <steve@boxjump.co.uk>
2014-10-09 11:19:15 +0100

    Use action in the general case

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -17,7 +17,7 @@ class Bottles
         "Go to the store and buy some more, #{count(remaining_next)} #{ subject(remaining_next) } of beer on the wall.\n"
     else
       "#{count(remaining).capitalize} #{ subject(remaining) } of beer on the wall, #{count(remaining)} #{ subject(remaining) } of beer.\n" <<
-        "Take #{pronoun(remaining)} down and pass it around, #{count(remaining_next)} #{ subject(remaining_next) } of beer on the wall.\n"
+        "#{action(remaining)}, #{count(remaining_next)} #{ subject(remaining_next) } of beer on the wall.\n"
     end
   end
 
