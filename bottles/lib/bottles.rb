a913e58058662f31b8d7c7f3421dad9830993e50
Steve Tooke <steve@boxjump.co.uk>
2014-10-09 10:23:29 +0100

    Make 8 bottles verse work using the remaining param

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,5 +1,5 @@
 class Bottles
   def verse(remaining)
-    "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
+    "#{remaining} bottles of beer on the wall, #{remaining} bottles of beer.\nTake one down and pass it around, #{remaining - 1} bottles of beer on the wall.\n"
   end
 end
