bedf8516987bff6ec9df1d9163f58c6dede69772
Steve Tooke <steve@boxjump.co.uk>
2014-10-09 11:11:38 +0100

    Use subject for the 0 bottles case

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -13,8 +13,8 @@ class Bottles
     remaining_next = remaining - 1
     case remaining
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\n" <<
-        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+      "No more #{ subject(remaining) } of beer on the wall, no more #{ subject(remaining) } of beer.\n" <<
+        "Go to the store and buy some more, 99 #{ subject(99) } of beer on the wall.\n"
     else
       "#{count(remaining)} #{ subject(remaining) } of beer on the wall, #{count(remaining)} #{ subject(remaining) } of beer.\n" <<
         "Take #{pronoun(remaining)} down and pass it around, #{count(remaining_next)} #{ subject(remaining_next) } of beer on the wall.\n"
