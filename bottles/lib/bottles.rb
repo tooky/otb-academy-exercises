681b65742065bca72cafcdcfe3378341f8d29af3
Steve Tooke <steve@boxjump.co.uk>
2014-10-09 10:27:36 +0100

    Make 2 bottles work

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,6 +2,8 @@ class Bottles
   def verse(remaining)
     if remaining == 1
       "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    elsif remaining == 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
     else
       "#{remaining} bottles of beer on the wall, #{remaining} bottles of beer.\nTake one down and pass it around, #{remaining - 1} bottles of beer on the wall.\n"
     end
