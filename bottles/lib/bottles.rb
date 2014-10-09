0d4601627470503898a6f2efdda07e65f4b7e471
Steve Tooke <steve@boxjump.co.uk>
2014-10-09 10:41:14 +0100

    Make the verses more readable

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -12,13 +12,17 @@ class Bottles
   def verse(remaining)
     case remaining
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+      "No more bottles of beer on the wall, no more bottles of beer.\n" <<
+        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" <<
+        "Take it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" <<
+        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     else
-      "#{remaining} bottles of beer on the wall, #{remaining} bottles of beer.\nTake one down and pass it around, #{remaining - 1} bottles of beer on the wall.\n"
+      "#{remaining} bottles of beer on the wall, #{remaining} bottles of beer.\n" <<
+        "Take one down and pass it around, #{remaining - 1} bottles of beer on the wall.\n"
     end
   end
 end
